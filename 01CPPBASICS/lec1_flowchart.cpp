6ab61347b7d3682076d306f05f11bcb88fb19334
Maulik Jain <jmaulik2006@gmail.com>
2026-06-07 20:43:38 +0530

    lec1 done. was about logics of basic programs and drawin flowcharts!

---
+++ 01CPPBASICS/lec1_flowchart.cpp
@@ -0,0 +1,14 @@
+//basic flow of probsolving
+//understand problem,
+// given values kya hai
+//best approach? code.
+
+
+
+//flowchart;       start->read->process->print->end
+
+//sum of n nos,  sum=0,num=1,sum=sum+num, num++
+//factorial,    prod=1,num=1,prod=prod*num, num++
+
+//flowchart for prime basic idea
+//suppose number is n, soo, (2 to n-1) take modulo of each with n, so n%_ and all,  !=0 for all, prime, or else, not prime
